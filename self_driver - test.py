6af0c6f2e90be8fe13902f5a82ce8c04a2493582
ParkSangJun <psj8252@naver.com>
2018-05-14 23:09:52 +0900

    정리

--- self_driver - test.py
+++ self_driver - test.py
@@ -5,13 +5,12 @@ from sendkey import ReleaseKey
 from time import sleep
 import tensorflow as tf
 import cv2
-from collections import deque
 import numpy as np
 import pandas as pd
 
 
 hidden_size = 4  # output from the LSTM. 5 to directly predict one-hot
-batch_size = 1   # one sentence
+batch_size = 100   # one sentence
 data_mean = 17.119790415671556
 data_std = 63.36037302373175
 
@@ -20,7 +19,9 @@ S = 0x1F
 D = 0x20
 SPACE = 0x39
 
-X = tf.placeholder(tf.float32, [5, 120, 300, 1])
+
+X = tf.placeholder(tf.float32, [None, 200, 400, 1])
+Y = tf.placeholder(tf.float32, [None, 4])
 
 with tf.variable_scope("cnn"):
     
@@ -37,25 +38,20 @@ with tf.variable_scope("cnn"):
     # pool3 
     pool3 = tf.layers.max_pooling2d(inputs=conv, pool_size=[4, 8], padding='SAME', strides=2)
     
-    pool3 = tf.reshape(pool, [1, 5, -1])
+    flat = tf.reshape(pool3, [-1, 100*200*16])
+    w = tf.get_variable("w", [hidden_size, hidden_size])
+    b = tf.get_variable("b", [hidden_size])
     
-with tf.variable_scope("rnn"):
-    cell = tf.contrib.rnn.LSTMCell(num_units=hidden_size, state_is_tuple=True)
+    logit = tf.layers.dense(inputs=flat, units=4)
+    outputs = tf.sigmoid(tf.matmul(logit, w) + b)
+<<<<<<< HEAD
+    predict = outputs - [0.5, 0.4, 0.5 , 0.10] > 0
+=======
+    predict = outputs - [0.2, 0.05,0.25 , 0.1] > 0
+>>>>>>> 0a8d469495bd36faf097215880ac675c0180672e
     
-    initial_state = cell.zero_state(batch_size, tf.float32)
-    #outputs : 1, 5, 50*99*16
-    outputs, _states = tf.nn.dynamic_rnn(cell, pool3, initial_state=initial_state, dtype=tf.float32)
-    
-    X_for_sigmoid = tf.reshape(outputs, [-1, hidden_size])
-    
-    sigmoid_w = tf.get_variable("softmax_w", [hidden_size, hidden_size])
-    sigmoid_b = tf.get_variable("softmax_b", [hidden_size])
-    outputs = tf.sigmoid(tf.matmul(X_for_sigmoid, sigmoid_w) + sigmoid_b)
-    
-    prediction = outputs > 0.5
-    
+
 saver = tf.train.Saver()
-last5_img = deque(maxlen=5)
 
 model = input("Drive Model Name: ")
 for i in range(5):
@@ -74,37 +70,13 @@ with tf.Session() as sess:
     while True:
 
         img = Image.open("./img/"+names[name_i])
-##        snapshot = np.array(ImageGrab.grab())
-##        _img = cv2.resize(snapshot, (400,200),interpolation=cv2.INTER_AREA)
-##        img = cv2.cvtColor(_img, cv2.COLOR_BGR2GRAY)
-##        img = cv2.Canny(img, threshold1 = 250, threshold2=200)
 
-        data = np.array( img, dtype='uint8' )
+        data = np.array( img, dtype='uint8' ).reshape([1,200,400,1])
         data = (data-data_mean) / data_std
-        last5_img.append(data)
 
-        if len(last5_img) < 5:
-            continue
-        
-        x_data = np.reshape(np.array(last5_img), [5, 120, 300, 1])
+        result, pred = sess.run([outputs, predict], feed_dict={X:data})
+        #x = sess.run(outputs, feed_dict={X:data})
+        print(key_out[name_i], [int(a*100)/100 for a in result[0]], pred)
 
-        x = sess.run(prediction, feed_dict={X:np.array(x_data, np.float32)})[-1]
-
-##        ReleaseKey(A)
-##        ReleaseKey(D)
-##        ReleaseKey(SPACE)
-##        ReleaseKey(S)
-        print(x, key_out[name_i], [int(a*100)/100 for a in sess.run(outputs, feed_dict={X:np.array(x_data, np.float32)})[-1]])
         name_i += 1
-##        if acts[1]:
-##            PressKey(S)
-##        if acts[0]:
-##            PressKey(A)
-##        if acts[2]:
-##            PressKey(D)
-##        if acts[3]:
-##            PressKey(SPACE)
-
-
-
         
--- self_driver.py
+++ self_driver.py
@@ -1,58 +1,53 @@
-from PIL import ImageGrab
+from PIL import ImageGrab, Image
 from getkeys import key_check
 from sendkey import PressKey
 from sendkey import ReleaseKey
 from time import sleep
 import tensorflow as tf
 import cv2
-from collections import deque
 import numpy as np
+import pandas as pd
 
-hidden_size = 16  # output from the LSTM. 5 to directly predict one-hot
-batch_size = 1   # one sentence
 
+hidden_size = 4  # output from the LSTM. 5 to directly predict one-hot
+batch_size = 100   # one sentence
+data_mean = 17.119790415671556
+data_std = 63.36037302373175
 
 A = 0x1E
 S = 0x1F
 D = 0x20
 SPACE = 0x39
 
-X = tf.placeholder(tf.float32, [5, 200, 400, 1])
+
+X = tf.placeholder(tf.float32, [None, 200, 400, 1])
+Y = tf.placeholder(tf.float32, [None, 4])
 
 with tf.variable_scope("cnn"):
     
     # conv : 5, 200, 400, 16
     conv = tf.layers.conv2d(inputs=X, filters=16, kernel_size=[4, 8], padding='SAME', activation=tf.nn.relu)
     # pool : 5, 50, 99, 16
-    pool = tf.layers.max_pooling2d(inputs=conv, pool_size=[2, 4], padding='VALID', strides=1)
+    pool = tf.layers.max_pooling2d(inputs=conv, pool_size=[2, 4], padding='SAME', strides=1)
     # conv2 
-    conv2 = tf.layers.conv2d(inputs=X, filters=64, kernel_size=[4, 8], padding='SAME', activation=tf.nn.relu)
+    conv2 = tf.layers.conv2d(inputs=pool, filters=32, kernel_size=[4, 8], padding='SAME', activation=tf.nn.relu)
     # pool2 
-    pool2 = tf.layers.max_pooling2d(inputs=conv, pool_size=[4, 8], padding='VALID', strides=2)
+    pool2 = tf.layers.max_pooling2d(inputs=conv2, pool_size=[4, 8], padding='SAME', strides=2)
     # conv3
-    conv2 = tf.layers.conv2d(inputs=X, filters=128, kernel_size=[4, 8], padding='SAME', activation=tf.nn.relu)
+    conv3 = tf.layers.conv2d(inputs=pool2, filters=64, kernel_size=[4, 8], padding='SAME', activation=tf.nn.relu)
     # pool3 
-    pool3 = tf.layers.max_pooling2d(inputs=conv, pool_size=[4, 8], padding='VALID', strides=2)
+    pool3 = tf.layers.max_pooling2d(inputs=conv3, pool_size=[4, 8], padding='SAME', strides=2)  
     
-    pool3 = tf.reshape(pool, [1, 5, -1])
+    flat = tf.reshape(pool3, [-1, 50*100*64])
+    w = tf.get_variable("w", [hidden_size, hidden_size])
+    b = tf.get_variable("b", [hidden_size])
     
-with tf.variable_scope("rnn"):
-    cell = tf.contrib.rnn.LSTMCell(num_units=hidden_size, state_is_tuple=True)
-    initial_state = cell.zero_state(batch_size, tf.float32)
-    #outputs : 1, 5, 50*99*16
-    outputs, _states = tf.nn.dynamic_rnn(cell, pool3, initial_state=initial_state, dtype=tf.float32)
+    logit = tf.layers.dense(inputs=flat, units=4)
+    outputs = tf.sigmoid(tf.matmul(logit, w) + b)
     
-    X_for_softmax = tf.reshape(outputs, [-1, hidden_size])
-    
-    softmax_w = tf.get_variable("softmax_w", [hidden_size, hidden_size])
-    softmax_b = tf.get_variable("softmax_b", [hidden_size])
-    outputs = tf.matmul(X_for_softmax, softmax_w) + softmax_b
     
 
-    prediction = tf.argmax(outputs, axis=1)
-    
 saver = tf.train.Saver()
-last5_img = deque(maxlen=5)
 
 model = input("Drive Model Name: ")
 for i in range(5):
@@ -63,35 +58,41 @@ for i in range(5):
 with tf.Session() as sess:
     saver.restore(sess, "./model/"+model+".ckpt")
 
+    data_df = pd.read_csv('log.csv', names=['name','key_out'])
+    names = data_df['name'].values
+    name_i = 0
+    key_out = [eval(x) for x in data_df['key_out'].values]
+
     while True:
+
         snapshot = np.array(ImageGrab.grab())
         _img = cv2.resize(snapshot, (400,200),interpolation=cv2.INTER_AREA)
         img = cv2.cvtColor(_img, cv2.COLOR_BGR2GRAY)
-        img = cv2.Canny(img, threshold1 = 250, threshold2=200)
 
-        data = np.array( img, dtype='uint8' )
-        last5_img.append(data)
-
-        if len(last5_img) < 5:
-            continue
+        data = np.array( img, dtype='uint8' ).reshape([1,200,400,1])
+        data = (data-data_mean) / data_std
+        result = sess.run(outputs, feed_dict={X:data})[0]
+        pred = result - [0.7, 0.5, 0.55, 0.10]
+        if pred[0]>pred[2]:
+            pred[2]=0
+        elif pred[2] > pred[0]:
+            pred[0]=0
+        pred = pred > 0
         
-        x_data = np.reshape(np.array(last5_img), [5, 200, 400, 1])
-
-        x = sess.run(prediction, feed_dict={X:np.array(x_data, np.float32)})[-1]
-        acts = [x//8, (x%8)//4, (x%4)//2, x%2]
-
+        #x = sess.run(outputs, feed_dict={X:data})
+        print(key_out[name_i], [int(a*100)/100 for a in result], pred)
         ReleaseKey(A)
         ReleaseKey(D)
         ReleaseKey(SPACE)
         ReleaseKey(S)
-        print(acts)
-        if acts[1]:
+        #print(x, key_out[name_i], [int(a*100)/100 for a in sess.run(outputs, feed_dict={X:data})])
+        if pred[1]:
             PressKey(S)
-        if acts[0]:
+        if pred[0]:
             PressKey(A)
-        if acts[2]:
+        if pred[2]:
             PressKey(D)
-        if acts[3]:
+        if pred[3]:
             PressKey(SPACE)
 
 
